dbf19ca8e778917b0ec6446c742c3beaff5021d0
Giulio Moretto - Promocare <giulio@promocare.it>
2024-02-05 11:53:57 +0100

    Aggiunta formattazione CLI, gestione frase vuota migliorata

--- src/main.py
+++ src/main.py
@@ -1,31 +1,53 @@
 import json
-import sys
 
+#Moduli Rich
+from rich import print
+from rich.panel import Panel
+from rich.text import Text
+from rich.console import Console
+from rich.prompt import Prompt
+
+#Moduli LLM
 from transformers import pipeline
+
+classifier = pipeline("token-classification", model = "sachaarbonel/bert-italian-cased-finetuned-pos")
+
+#console per fomattazione errore
+error_console = Console(stderr=True, style="bold red")
 
 #Preleva struttura DB da file
 dbFile = open("db.json")
 db = json.load(dbFile)
 dbFile.close()
-
+#Preleva tabelle
 tables = db['database']['tabelle']
 
-#Chiedi frase in linguaggio naturale
-naturalLangPhrase = input("Inserisci frase di interrograzione in linguaggio naturale: ")
+#Benvenuto
+print(Panel(Text("Benvenuti in ChatSQL!\nInserite una frase per interrogare il database.",justify="center"), title="ChatSQL"))
+naturalLangPhrase = ""
 
-if len(naturalLangPhrase)==0:
-    sys.exit("Errore: Frase in linguaggio naturale non inserita.")
+
+while(len(naturalLangPhrase)==0):
+    #Chiedi frase in linguaggio naturale
+    try:
+        naturalLangPhrase = Prompt.ask("[bold blue]Inserisci frase")
+    except KeyboardInterrupt:
+        error_console.print("Programma interrotto. Uscita in corso")
+        exit()
+
+    if len(naturalLangPhrase)==0:
+        error_console.print("Errore: Frase in linguaggio naturale non inserita.")
 
 #Identifica le parti del linguaggio (Parts of speech)
-classifier = pipeline("token-classification", model = "sachaarbonel/bert-italian-cased-finetuned-pos")
+print("[blue]Generazione prompt...[/blue]")
 tokens = classifier(naturalLangPhrase)
 
 if(len(tokens)==0):
-    sys.exit("Errore: impossibile interpretare la frase inserita.")
+    error_console.print("Errore: impossibile interpretare la frase inserita.")
+    exit()
 
 #Preleva i nomi
 nouns = []
-
 for token in tokens:
     if token['entity'] == 'NOUN':
         nouns.append(token['word'])
@@ -61,6 +83,10 @@ if len(foundTables)>0:
     prompt += "Crea una query SQL per la seguente richiesta: "+naturalLangPhrase
 
 else:
-    print("Errore: Frase non inerente al database.")
+    error_console.print("Errore: Frase non inerente al database.")
+    exit()
 
-print(prompt)
+#Stampa prompt e istruzioni
+print(Text("Prompt Generato:\n",style="blue"))
+print("[italic green]"+prompt+"\n[/italic green]")
+print("Copia il testo generato in https://chat.openai.com/ per ricere la query SQL")
