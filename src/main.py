c19b275f068f30f07fe01b9df02488a26d117a78
Metz01 <camillomatteo01@gmail.com>
2024-02-13 11:20:35 +0100

    Added choose between dbs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import json
 
+import logging
+
 #Moduli Rich
 from rich import print
 from rich.panel import Panel
@@ -10,6 +12,19 @@ from rich.prompt import Prompt
 #Moduli LLM
 from transformers import pipeline
 
+# Set the logging level to only show errors
+logging.basicConfig(level=logging.ERROR)
+
+def load_all_databases():
+    # Load the JSON data from the file
+    dbFile = open("db.json", "r")
+    data = json.load(dbFile)
+    dbFile.close()
+
+    # Get names of all databases
+    database_names = list(data.keys())
+    return database_names
+
 classifier = pipeline("token-classification", model = "sachaarbonel/bert-italian-cased-finetuned-pos")
 
 #console per fomattazione errore
@@ -19,13 +34,39 @@ error_console = Console(stderr=True, style="bold red")
 dbFile = open("db.json")
 db = json.load(dbFile)
 dbFile.close()
-#Preleva tabelle
-tables = db['database']['tabelle']
 
 #Benvenuto
 print(Panel(Text("Benvenuti in ChatSQL!\nInserite una frase per interrogare il database.",justify="center"), title="ChatSQL"))
-naturalLangPhrase = ""
 
+# List of available databases
+all_databases = load_all_databases()
+
+# Database selection
+print("Available Databases:")
+for index, db_name in enumerate(all_databases, start=1):
+    print(f"{index}. {db_name}")
+
+try:
+    selected_db_index = Prompt.ask("[bold blue]Select a database by entering its number")
+except KeyboardInterrupt:
+    error_console.print("Program interrupted. Exiting.")
+    exit()
+
+try:
+    selected_db_index = int(selected_db_index)
+    selected_db_name = all_databases[selected_db_index - 1]
+except (ValueError, IndexError):
+    error_console.print("Error: Invalid database selection.")
+    exit()
+
+# Load the selected database
+with open("db.json", "r") as json_file:
+    data = json.load(json_file)
+
+selected_db = data[selected_db_name]
+tables = selected_db['tabelle']
+
+naturalLangPhrase = ""
 
 while(len(naturalLangPhrase)==0):
     #Chiedi frase in linguaggio naturale
