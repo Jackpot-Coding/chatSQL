583b37a5481cfe84adf8e017784acccfb006d4f2
Marco-Favaretto <favaretto801@gmail.com>
2024-03-14 12:25:58 +0100

    sistemazione creazione tabella

--- docs/src/esterni/TabTexFromFile.java
+++ docs/src/esterni/TabTexFromFile.java
@@ -6,23 +6,43 @@ import java.util.Scanner;
 
 public class TabTexFromFile {
 
+    static int countLines(Scanner sc) {
+        int count = 0;
+        while (sc.hasNextLine()) {
+            count++;
+            sc.nextLine();
+        }
+        return count;
+    }
+
     static String rowToLatex(String row[]) {
         String s = "          " ;
         for(int i = 0; i < row.length-1; i++) {
-            s = s + row[i] + " & ";
+            s =  s + row[i] + " & ";
         }
         s = s + row[row.length-1] + " \\\\ \\hline";
         return s;
     }
 
-    static String getRow(String row) {
+    static String getRow(String row, boolean firstRow) {
         String s[] = row.split("\t");
+        if(firstRow) {
+            for(int i = 0; i < s.length; i++) s[i] = "\\textbf{" + s[i] + "}";
+        }
         return rowToLatex(s);
     }
 
-    static void printTab(Scanner sc) {
+    static void printTab(Scanner sc, int lines) {
+        int word = 0;
+        String rows[] = new String[lines];
         while (sc.hasNextLine()) {
-            System.out.println(getRow(sc.nextLine()));
+            rows[word] = sc.nextLine();
+            word++;
+        }
+        for(int i = 0; i < lines; i++) {
+            boolean firstRow = false;
+            if(i == 0) firstRow = true; 
+            System.out.println(getRow(rows[i], firstRow));
         }
     }
 
@@ -31,9 +51,9 @@ public class TabTexFromFile {
                         "      \\centering\r\n" + //
                         "      \\rowcolors{2}{black!15}{}\r\n" + //
                         "      \\resizebox{\\linewidth}{!}{\r\n" + //
-                        "        \\begin{tabular}{|c|c|c|c|c|c|c||c|}\r\n" + //
+                        "        \\begin{tabular}{|c|c|c|c|c|c|c|c|}\r\n" + //
                         "          \\hline\r\n" + //
-                        "          \\rowcolor{yellow!50}");
+                        "          \\rowcolor{teal!50}");
     }
 
     static void printLast() {
@@ -47,9 +67,9 @@ public class TabTexFromFile {
         File file = new File(
             "docs\\src\\esterni\\tab.txt");
         try {
-            
+
             printFirst();
-            printTab(new Scanner(file));
+            printTab(new Scanner(file), countLines(new Scanner(file)));
             printLast();
             
         } catch (FileNotFoundException e) {
