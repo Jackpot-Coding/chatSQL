3bde4636b151008f9cb76ccb6600b94106be6a52
Marco-Favaretto <favaretto801@gmail.com>
2024-04-16 16:09:59 +0200

    scheletro forms.py e html

--- src/chatSQL/admin/forms.py
+++ src/chatSQL/admin/forms.py
@@ -8,6 +8,12 @@ class StrutturaDatabaseForm(forms.Form):
     nome = forms.CharField(label="Nome")
     descrizione = forms.CharField(widget=forms.Textarea, label="Descrizione")
     
+class TabellaForm(forms.Form):
+    nome = forms.CharField(max_length=255, required=True, label="Nome")
+    descrizione = forms.CharField(widget=forms.Textarea, label="Descrizione")
+    sinonimi = forms.CharField(max_length=255, required=False, label="Sinonimi")
+    struttura = forms.CharField(max_length=255, required=True, label="Struttura DB")    
+    
 class EliminaForm(forms.Form):
     classe_modello = forms.CharField(widget=forms.HiddenInput)
     id_modello = forms.IntegerField(widget=forms.HiddenInput)
