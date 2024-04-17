b0ab39497b37130d7e0abd7bc6855f3789917959
Marco-Favaretto <favaretto801@gmail.com>
2024-04-17 13:34:08 +0200

    correzione test creazione tabella

--- src/chatSQL/admin/tests/test_tabella.py
+++ src/chatSQL/admin/tests/test_tabella.py
@@ -7,29 +7,29 @@ from ..models import Tabella
 from ..models import StrutturaDatabase
 
 class CreateTableTestCase(TestCase):
-    client = Client()
-
+    client = Client() # client per eseguire get e post
+    db = StrutturaDatabase(nome='Test Str', descrizione='Test Db Descrizione') # db di test
+    
     def setUp(self):
-        User.objects.create_user(username="testAdmin",password="testPassword123!")
+        User.objects.create_user(username="testAdmin", password="testPassword123!")
         self.client.login(username='testAdmin', password='testPassword123!')
+        self.db.save() # salva il db
         
     def test_can_reach_table_creation_page(self):
-        response = self.client.get(reverse('new_table_view'))
-        
+        # passa come argomento la pk del db
+        response = self.client.get(reverse('new_table_view', args=(self.db.pk,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'admin/tabella.html')
         
     def test_can_create_table(self):
-        data_db = {'nome': 'Test Tab Str', 'descrizione': 'Test Db Descrizione'}
-        self.client.post(reverse('new_db_view'), data_db)
-        
-        db_id = StrutturaDatabase.objects.filter(nome=data_db.get('nome')).first()
         
         data = {'nome': 'Test Table', 
                 'descrizione': 'Test Table Description', 
                 'sinonimi': 'Test Sinonimi', 
-                'struttura': db_id.pk
-            }
-        response = self.client.post(reverse('new_table_view'), data)
+            }                                   # passa come argomento la pk del db alla post, e i dati mancanti per creare la tabella
+        response = self.client.post(reverse('new_table_view', args=(self.db.pk,)), data)
         self.assertEqual(response.status_code, 200) #tabella creata
-        self.assertTrue(Tabella.objects.filter(nome='Test Table').exists())
+        self.assertTrue(Tabella.objects.filter(nome='Test Table').exists()) # tabella corretta
+        
+#class TabellaFormTestCase(TestCase):
+    
