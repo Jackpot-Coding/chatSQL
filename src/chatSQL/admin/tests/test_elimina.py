d704c112648251e94428372ec8924b939fd62b63
Riccardo Simionato <riccardo.simionato13@gmail.com>
2024-04-19 09:28:49 +0200

    aggiunti test per eliminazione tabella e campo

--- src/chatSQL/admin/tests/test_elimina.py
+++ src/chatSQL/admin/tests/test_elimina.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
 
 from ..forms import EliminaForm
-from ..models import StrutturaDatabase
+from ..models import StrutturaDatabase, Tabella, Campo
 
 class adminTestCase(TestCase):
     
@@ -17,6 +17,10 @@ class adminTestCase(TestCase):
         
         struttura_db = StrutturaDatabase(nome="Test DB",descrizione="Descrizione test")
         struttura_db.save()
+        tab_test = Tabella(nome="Test Tabella", descrizione="Descrizione test", struttura=struttura_db)
+        tab_test.save()
+        campo = Campo(nome="Test Tabella", tipo="INT", descrizione="Descrizione test", tabella=tab_test)
+        campo.save()
     
     def test_can_render_struttura_database_deletion_form(self):
         
@@ -36,6 +40,26 @@ class adminTestCase(TestCase):
         self.assertEqual(response.status_code,302)
         self.assertRedirects(response,reverse('admin_home'))
         self.assertTrue("Struttura database eliminata" in messages[0].message)
+
+    def test_can_delete_tabella(self):
+        url = reverse("model_delete",kwargs={"classe_modello":"Tabella","id_modello":1})
+        response = self.client.post(url,data={"classe_modello":"Tabella","id_modello":"1"})
+
+        messages = [msg for msg in get_messages(response.wsgi_request)]
+
+        self.assertEqual(response.status_code,302)
+        self.assertRedirects(response,reverse("db_view", kwargs={'structure_id':1}))
+        self.assertTrue("Tabella eliminata" in messages[0].message)
+
+    def test_can_delete_campo(self):
+        url = reverse("model_delete",kwargs={"classe_modello":"Campo","id_modello":1})
+        response = self.client.post(url,data={"classe_modello":"Campo","id_modello":"1"})
+
+        messages = [msg for msg in get_messages(response.wsgi_request)]
+
+        self.assertEqual(response.status_code,302)
+        self.assertRedirects(response,reverse("table_view", kwargs={'table_id':1}))
+        self.assertTrue("Campo eliminato" in messages[0].message)
         
     def test_cannot_delete_not_existing_struttura_database(self):
         url = reverse("model_delete",kwargs={"classe_modello":"StrutturaDatabase","id_modello":100})
