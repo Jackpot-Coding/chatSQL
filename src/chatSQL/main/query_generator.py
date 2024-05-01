a6efa6cbe89930b290944c76786b563a02b89489
Giulio Moretto - Promocare <giulio@promocare.it>
2024-05-01 21:31:24 +0200

    Aggiunta istruzione di brevità per modello in QueryGenerator

--- src/chatSQL/main/query_generator.py
+++ src/chatSQL/main/query_generator.py
@@ -12,6 +12,7 @@ class QueryGenerator:
                     model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
                     messages=[
                         {"role": "system", "content": "Do not explain code."},
+                        {"role": "system", "content": "Give an executive summary"},
                         {"role": "system", "content": "Always replay in Italian."},
                         {"role": "system", "content": "Always wrap code in a markdown code block"},
                         {"role": "user", "content": prompt}
