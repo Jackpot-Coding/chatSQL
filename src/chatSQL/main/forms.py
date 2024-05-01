27428e7e3ef662fe1e537f42aaf263909911e1c6
Giulio Moretto - Promocare <giulio@promocare.it>
2024-05-01 21:12:07 +0200

    Correzione labels NLPromptForm e Sumbit->Salva in modifica campo

--- src/chatSQL/main/forms.py
+++ src/chatSQL/main/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from admin.models import StrutturaDatabase
 
 class NLPromptForm(forms.Form):
-    natural_language = forms.CharField(label='Natural Language', widget=forms.Textarea)
-    db_structure = forms.ModelChoiceField(queryset=StrutturaDatabase.objects.all(), label='Database Structure')
+    natural_language = forms.CharField(label='Frase in linguaggio naturale', widget=forms.Textarea)
+    db_structure = forms.ModelChoiceField(queryset=StrutturaDatabase.objects.all(), label='Struttura Database')
     
 class QueryForm(forms.Form):
     prompt = forms.CharField(label='Prompt', widget=forms.HiddenInput)
